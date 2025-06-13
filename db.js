55a1d59ee4dafe8b303909ce97af53eb61763671
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2025-06-13 09:04:47 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './database.sqlite'
+    storage: 'Projeto_Biblioteca_Web/database.sqlite'
 });
 
 module.exports = sequelize;
