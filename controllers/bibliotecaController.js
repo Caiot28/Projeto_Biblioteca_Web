c58c87bffcf2956ae7b8a54779a9df4e082de97d
Maria Eduarda Ribeiro Gabriel <mariaeduardaribeiro0212@gmail.com>
2025-06-23 21:33:19 -0300

    botão buscar e alterações finais

--- controllers/bibliotecaController.js
+++ controllers/bibliotecaController.js
@@ -1,78 +1,78 @@
 const Livro = require('../models/livroModels');
 
-function getIndexView(req, res){
+function getIndexView(req, res) {
     res.render('index.html');
 }
 
-function getCadastroLivroView(req, res){
+function getCadastroLivroView(req, res) {
     res.render('cadastro.html');
 }
 
-function getListarView(req, res){
-    Livro.findAll().then((livros)=>{
-        res.render('listagem_livros.html', {livros});
+function getListarView(req, res) {
+    Livro.findAll().then((livros) => {
+        res.render('listagem_livros.html', { livros });
     })
 }
 
-async function postCadastrarLivro(req, res){
+async function postCadastrarLivro(req, res) {
     let dados_livro = req.body;
     let campos_invalidos = await validarCadastroLivro(dados_livro);
 
-    if(campos_invalidos.length == 0){
-        Livro.create(dados_livro).then(()=>{
+    if (campos_invalidos.length == 0) {
+        Livro.create(dados_livro).then(() => {
             res.redirect('/listar');
         });
     }
-    else{
-        res.render('cadastro.html', {campos_invalidos, dados_livro});
+    else {
+        res.render('cadastro.html', { campos_invalidos, dados_livro });
     }
 }
 
-function getEditarLivro(req, res){
+function getEditarLivro(req, res) {
     let isbn_livro = req.params.isbn;
     Livro.findOne({
-        where:{
+        where: {
             isbn: isbn_livro
         }
-    }).then((dados_livro)=>{
-        res.render('editar_livro.html', {dados_livro});
-    }); 
-    
+    }).then((dados_livro) => {
+        res.render('editar_livro.html', { dados_livro });
+    });
+
 }
 
-async function postEditarLivro(req, res){
+async function postEditarLivro(req, res) {
     let dados_livro = req.body;
     let campos_invalidos = await validarCadastroLivro(dados_livro);
 
-    if(campos_invalidos.length == 0){
+    if (campos_invalidos.length == 0) {
         await Livro.findOne({
-            where:{
+            where: {
                 //isbn: dados_livro.isbn
                 id: dados_livro.id
             }
-        }).then((dados_cadastro)=>{
-            dados_cadastro.update(dados_livro).then(()=>{
+        }).then((dados_cadastro) => {
+            dados_cadastro.update(dados_livro).then(() => {
                 res.redirect('/listar');
             });
-            
-        }); 
+
+        });
     }
-    else{
-        res.render('editar_livro.html', {campos_invalidos, dados_livro});
+    else {
+        res.render('editar_livro.html', { campos_invalidos, dados_livro });
     }
 }
 
-function getExcluirLivro(req, res){
+function getExcluirLivro(req, res) {
     let isbn_livro = req.params.isbn;
     Livro.findOne({
-        where:{
+        where: {
             isbn: isbn_livro
         }
-    }).then((dados_livro)=>{
-        dados_livro.destroy().then(()=>{
+    }).then((dados_livro) => {
+        dados_livro.destroy().then(() => {
             res.redirect('/listar');
         });
-    }); 
+    });
 }
 
 async function validarCadastroLivro(dados_livro) {
@@ -99,15 +99,15 @@ async function validarCadastroLivro(dados_livro) {
     if (dados_livro.qtd_estoque == null || dados_livro.qtd_estoque == undefined || dados_livro.qtd_estoque == '') {
         campos_invalidos.push("Quantidade no estoque inválido! Não pode ser vazio.");
     }
-    if (dados_livro.isbn.length > 0) { 
+    if (dados_livro.isbn.length > 0) {
         try {
             const livroExistente = await Livro.findOne({
                 where: {
                     isbn: dados_livro.isbn
                 }
             });
- 
-            if (livroExistente) {  
+
+            if (livroExistente) {
                 if (dados_livro.id != livroExistente.id) {
                     campos_invalidos.push("ISBN já cadastrado");
                 }
@@ -121,6 +121,23 @@ async function validarCadastroLivro(dados_livro) {
     return campos_invalidos;
 }
 
+async function buscarPorIsbn(req, res) {
+    const isbnBuscado = req.query.isbn;
+
+    try {
+        const livro = await Livro.findOne({ where: { isbn: isbnBuscado } });
+
+        if (livro) {
+            res.render('listagem_livros', { livros: [livro] }); 
+        } else {
+            res.render('listagem_livros', { livros: [], mensagem: 'Nenhum livro encontrado com este ISBN.' });
+        }
+
+    } catch (error) {
+        console.error('Erro na busca por ISBN:', error);
+        res.status(500).send('Erro ao buscar livro.');
+    }
+}
 module.exports = {
     getIndexView,
     getCadastroLivroView,
@@ -128,5 +145,6 @@ module.exports = {
     getListarView,
     postEditarLivro,
     getExcluirLivro,
-    getEditarLivro
+    getEditarLivro,
+    buscarPorIsbn
 }
--- routers/bibliotecaRouter.js
+++ routers/bibliotecaRouter.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const bibliotecaController = require('../controllers/bibliotecaController');
+const { verificarAutenticacao } = require('../controllers/usuarioController');
+
+router.get('/menu', verificarAutenticacao, (req, res) => {
+  res.render('menu', { nomeUsuario: req.session.usuario.nome });
+});
 
 router.get('/', bibliotecaController.getIndexView);
-router.get('/cadastro', bibliotecaController.getCadastroLivroView);
-router.get('/listar', bibliotecaController.getListarView);
-router.post('/cadastrar_livro', bibliotecaController.postCadastrarLivro);
-router.get('/editar_livro/:isbn', bibliotecaController.getEditarLivro);
-router.post('/editar_livro', bibliotecaController.postEditarLivro);
-router.get('/excluir_livro/:isbn', bibliotecaController.getExcluirLivro);
-
+router.get('/cadastro', verificarAutenticacao, bibliotecaController.getCadastroLivroView);
+router.get('/listar', verificarAutenticacao, bibliotecaController.getListarView);
+router.post('/cadastrar_livro', verificarAutenticacao, bibliotecaController.postCadastrarLivro);
+router.get('/editar_livro/:isbn', verificarAutenticacao, bibliotecaController.getEditarLivro);
+router.post('/editar_livro', verificarAutenticacao, bibliotecaController.postEditarLivro);
+router.get('/excluir_livro/:isbn', verificarAutenticacao, bibliotecaController.getExcluirLivro);
+router.get('/buscar', verificarAutenticacao, bibliotecaController.buscarPorIsbn);
 
 
 module.exports = router;
